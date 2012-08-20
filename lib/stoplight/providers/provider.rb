d25d9a514ba2fae734c1cb09a69434d5ca19f661
Daniel Johnson <navseeker7@gmail.com>
2012-08-19 21:40:37 -0700

    logger hack because im tired

--- lib/stoplight/providers/provider.rb
+++ lib/stoplight/providers/provider.rb
@@ -5,10 +5,10 @@ require 'httparty'
 require 'rack/commonlogger'
 require 'uri'
 
-unless $logger
-  require 'logger'
-  $logger = Logger.new('log/application.log')
-end
+#unless $logger
+#  require 'logger'
+#  $logger = Logger.new('log/application.log')
+#end
 
 # Provider is an abstract class that all providers inherit from. It requires that a specified format be returned. This way, stoplight
 # doesn't care who it's talking to, as long as it guarantees certain information.
